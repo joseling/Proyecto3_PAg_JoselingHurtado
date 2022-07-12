233a1b2b7378cee2c4ad0613ae9b67071ea53dde
Josseling Hurtado <64708205+joseling@users.noreply.github.com>
2022-07-13 00:06:44 +0200

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,4 @@
+const carrito = document.getElemntById('carrito');
+const curso = document.getElementById('lista-curso');
+const listacurso=document.getElementById('#lista-carrito tbody');
+const vaciarcarritoBtn = document.getElementById('vaciar-carrito');
